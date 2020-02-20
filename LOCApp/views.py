1aed7181a13f04251fc3de60d7b1fe869783ef95
amoghparab1805 <mumbai.amoghparab@gmail.com>
2020-02-21 02:04:48 +0530

    removed signup permission

--- LOCApp/views.py
+++ LOCApp/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
 from rest_framework import permissions
+from rest_framework.decorators import permission_classes
 from rest_framework.generics import CreateAPIView
 from . serializers import UserSerializer
 from . models import User
 
+@permission_classes((permissions.AllowAny, ))
 class CreateUserView(CreateAPIView):
     model = User
     serializer_class = UserSerializer
